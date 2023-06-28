09720016d42f1ad11feebcc981a51be737012bb3
João Vytor Manzano <manzanogamermc@gmail.com>
2023-06-27 22:00:25 -0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = 'fcef93db-1fb5-4981-9641-19153547273d';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
